0756aebe7754d0d4f01ca53e7d93dedf492c5db9
DOSOL\dosol <dosol48626@naver.com>
2024-10-09 16:30:12 +0900

    10.09 DTO까지 만듬.

---
+++ src/main/java/com/dosol/springcoreh/sample/SampleDTO.java
@@ -0,0 +1,14 @@
+package com.dosol.springcoreh.sample;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SampleDTO {
+    private String name;
+    private int age;
+    private String[] hobby;
+}
